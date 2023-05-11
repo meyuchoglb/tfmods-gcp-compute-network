ec52819831d3179d3b434de725c1d3d57d94d918
Ismael Ull <ismael.ull@paack.co>
2023-05-11 11:41:33 -0300

    Adding Variables

---
+++ variables.tf
@@ -0,0 +1,32 @@
+# --------------------------------------------------------------------------------
+#                                  Variables
+# --------------------------------------------------------------------------------
+
+variable "project_id" {
+  type        = string
+  description = "GCP project ID."
+}
+
+variable "vpc_name" {
+  type        = string
+  default     = "primary"
+  description = "GCP VPC name."
+}
+
+variable "routing_mode" {
+  type        = string
+  default     = "GLOBAL"
+  description = "GCP network routing mode to use."
+}
+
+variable "autocreate_subnetworks" {
+  type        = bool
+  description = "Allow the automatic creation of subnetworks"
+  default     = {false}
+}
+
+variable "mtu" {
+  type        = string
+  description = "VPC's MTU"
+  default     = {1460}
+}
